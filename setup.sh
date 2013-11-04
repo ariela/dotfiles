782d1a402c59d8597a50b143da046860aad1aa45
yuki <yuki@editor.ariela.jp>
2013-11-04 15:42:46 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git submodule init
+git submodule update
+
+create_link() {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    echo "Install:	$target"
+    ln -sf ${source} ${target}
+}
+
+
+for i in _*
+do
+    create_link $i
+done
+create_link bin
